d5a6bfcb75bb188d6677911bad4f551f0f53a5b6
Sly Vindictus <coolboyhd2014@gmail.com>
2022-10-20 08:30:33 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,17 @@
-print ("1. Legg inn ny annsatt")
-print ("2. Rediger ansatt")
-print ("3. Slett ansatt")
-print ("4. List ut alle ansatte")
-print ("5. Avslutt")
+def legg_inn():
+    print ("legg in ny annsatt valgt")
+    fornavn =input("skriv inn fornavn: ")
+run=True
+while run:
+    print ("1. Legg inn ny annsatt")
+    print ("2. Rediger ansatt")
+    print ("3. Slett ansatt")
+    print ("4. List ut alle ansatte")
+    print ("5. Avslutt")
 
-valg=input("velg hva som du vil gjøre")
-print(valg)
+    valg=input("velg hva som du vil gjøre: ")
+    print(valg)
+    if valg== "1":
+        legg_inn()
+    else:
+        print ("Feil")
